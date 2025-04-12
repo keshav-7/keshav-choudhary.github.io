098803f14665dda880278555824c0c7adda6f73e
keshav-77 <keshav.choudhary@healthians.com>
2025-04-13 02:26:53 +0530

    My Portfoli - 04

--- configs.js
+++ configs.js
@@ -1,3 +1,3 @@
 const EMAILJS_PUBLIC_KEY = "nCFxPTbgSgUy4PO5n";
 const EMAILJS_SERVICE_ID = "service_zixma9k";
-const EMAILJS_TEMPLATE_ID = "template_ncluvil---";
+const EMAILJS_TEMPLATE_ID = "template_ncluvil";
